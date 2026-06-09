51658c95a4b2b7390b32026a8974d4b67c91c848
thomaslucking <telucking@gmail.com>
2026-06-09 13:01:20 +0200

    feat: users must verify their email now

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,10 +1,10 @@
-@props(['title', 'value', 'type' => 'text'])
+@props(['title', 'name', 'value' => null, 'type' => 'text'])
 
 <div>
-    <label for="title" class="block text-base font-medium text-gray-700">{{ $title }}</label>
-    <input type="{{ $type }}" id="title" name="title" value="{{ $value ?? old('title') }}"
-        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-base @error('title')  @enderror">
-    @error('title')
+    <label for="{{ $name }}" class="block text-base font-medium text-gray-700">{{ $title }}</label>
+    <input type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" value="{{ $value ?? old($name) }}"
+        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-base @error($name) cle @enderror">
+    @error($name)
         <p class="mt-1 text-base text-red-600">{{ $message }}</p>
     @enderror
-</div> @enderror">{{  }}
+</div>
