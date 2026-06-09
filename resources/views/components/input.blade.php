ef92d772ebb5b3b618d709396e13be5e2727da3f
thomaslucking <telucking@gmail.com>
2026-06-09 12:56:58 +0200

    fix: corrected css warnings

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -3,8 +3,8 @@
 <div>
     <label for="title" class="block text-base font-medium text-gray-700">{{ $title }}</label>
     <input type="{{ $type }}" id="title" name="title" value="{{ $value ?? old('title') }}"
-        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-base @error('title') border-red-500 @enderror">
+        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-base @error('title')  @enderror">
     @error('title')
         <p class="mt-1 text-base text-red-600">{{ $message }}</p>
     @enderror
-</div>
+</div> @enderror">{{  }}
