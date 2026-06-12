6ad78e97b5363d4b57361bdc0b7a8e76f8a47dfe
igortrajic <iggetrajic@gmail.com>
2026-06-12 11:54:56 +0200

    feat: added half of the migrations (#69)
    * feat: added half of the migrations
    
    closes #9
    
    * fix: migartion fixes
    
    foreign key renamed correctly
    also removed ,50 from integer for work_rate

---
+++ database/migrations/2026_06_12_090814_project_phases.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('project_phases', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->string('duration', 100)->nullable();
+            $table->json('objectifs')->nullable();
+            $table->json('livrables')->nullable();
+            $table->integer('order')->default(0);
+            
+            $table->foreignId('project_id')
+                  ->constrained('projects')
+                  ->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('project_phases');
+    }
+};
+
+
