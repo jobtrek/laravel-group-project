61e2f97c845234e7081592cba1279cd1152d977f
nekrosb <makarov.nikita.mn@gmail.com>
2026-06-12 13:18:25 +0200

    feat: add models (#71)
    * feat: add models
    
    * fix: change type of fillable on protected

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use app\Models\User;
 
-class comment extends Model
+class Comment extends Model
 {
     /** @use HasFactory<\Database\Factories\CommentFactory> */
     use HasFactory;
+    protected $fillable = ['content', 'stage', 'user_id', 'project_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
---
+++ app/Models/Project_evaluation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use app\Models\User;
+use app\Models\Project;
+
+class project_evaluation extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProjectEvaluationFactory> */
+    use HasFactory;
+    protected $fillable = ['portee', 'impact', 'confiance', 'effort', 'importance', 'project_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
