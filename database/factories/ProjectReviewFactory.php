eaea6c37a5b9b3338d8c2425b62e7ad7c2895fa0
nekrosb <makarov.nikita.mn@gmail.com>
2026-06-12 11:46:13 +0200

    feat: add migration (#67)

---
+++ database/factories/ProjectReviewFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\project_review;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<project_review>
+ */
+class ProjectReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
