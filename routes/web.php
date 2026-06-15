0154b15ed0a1ef1e902aa68874af691eab43deb4
FrstF4ll <hydralixe@gmail.com>
2026-06-15 10:01:36 +0200

    feat: extract creation page to a different component
    - Add new route to creation page
    - Remove creation details from dashboard page
    - Add 'create' to navigation bar

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/create', function () {
+    return view('create');
+})->middleware(['auth', 'verified'])->name('create');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
