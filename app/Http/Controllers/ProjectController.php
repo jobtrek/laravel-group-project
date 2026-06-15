caf76df728e5b4d67f657329276154e9e2309cf1
nekrosb <makarov.nikita.mn@gmail.com>
2026-06-15 13:16:30 +0200

    feat(backend): add router for listing of projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Project;
+use app\Models\User;
 
 class ProjectController extends Controller
 {
     public function listingProjects()
     {
         $projects = Project::select('id', 'title', 'description')->with("evaluations")->get();
+        $users = User::query()->select('id', 'name')->get();
+
+        return view('dashboard', ['projects' => $projects, 'users' => $users]);
     }
 }
