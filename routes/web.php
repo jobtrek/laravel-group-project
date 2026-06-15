8bb4364aa8addaf1f4ab884a8ffac8b9f85983c8
FrstF4ll <hydralixe@gmail.com>
2026-06-15 15:02:29 +0200

    refactor: group dashboard and create routes

--- routes/web.php
+++ routes/web.php
@@ -9,19 +9,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/create', function () {
-    return view('create', ['users' => User::query()->select('id', 'name')->get()]);
-})->middleware(['auth', 'verified'])->name('create');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+    Route::get('/create', function () {return view('create', ['users' => User::query()->select('id', 'name')->get()]);})->name('create');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::post('/propositions', [PropositionController::class, 'store'])->name('proposition.store');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
