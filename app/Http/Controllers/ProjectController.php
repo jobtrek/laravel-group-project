dddf71bad05bcf5e71b15ee9d2c388e7d1c6ba8b
FrstF4ll <hydralixe@gmail.com>
2026-06-15 14:27:12 +0200

    style: style error and format

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -9,7 +9,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::select('id', 'title', 'description')->with("evaluation")->get();
+        $projects = Project::select('id', 'title', 'description')->with('evaluation')->get();
         $users = User::select('id', 'name')->get();
 
         return view('dashboard', ['projects' => $projects, 'users' => $users]);
--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,4 @@ Route::middleware('auth')->group(function () {
     Route::post('/propositions', [PropositionController::class, 'store'])->name('proposition.store');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
