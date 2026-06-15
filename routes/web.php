7c29e53682e76a7cb773ef7a7dfff936bc01fa77
nekrosb <makarov.nikita.mn@gmail.com>
2026-06-15 13:52:54 +0200

    feat(backend): add listing of projects (#76)

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,18 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PropositionController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\ProjectController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard', ['users' => User::query()->select('id', 'name')->get()]);
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+Route::get('/dashboard', [ProjectController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::post('/propositions', [PropositionController::class, 'store'])->name('proposition.store');
-
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
