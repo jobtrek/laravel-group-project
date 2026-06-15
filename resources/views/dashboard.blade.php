71e88b496b9e7993d2027728ceee01f5a5f4a4c4
FrstF4ll <hydralixe@gmail.com>
2026-06-15 14:06:58 +0200

    feat: build dashboard as a list displayer
    - For the moment, just copy and past preview, later use a foreach loop for rendering

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,8 @@
 <x-app-layout>
-Projects will show up there...
+    <section class="flex flex-col gap-4 p-4">
+        <x-preview/>
+        <x-preview/>
+        <x-preview/>
+        <x-preview/>
+    </section>
 </x-app-layout>
