28981d570d20ded333bc985a4a9a60acc607d02c
nekrosb <makarov.nikita.mn@gmail.com>
2026-06-19 13:30:08 +0200

    fix: remove block of code not needed

--- app/Http/Controllers/RecolteController.php
+++ app/Http/Controllers/RecolteController.php
@@ -14,36 +14,6 @@ class RecolteController extends Controller
             ->with('phases.resources')
             ->get();
 
-        // 2. go through each project and calculate the progress based on the resources needed and found
-        foreach ($projects as $project) {
-            $totalNeeded = 0;
-            $totalFound = 0;
-            $progress = 0;
-
-            foreach ($project->phases as $phase) {
-
-                foreach ($phase->resources as $resource) {
-
-                    $totalNeeded += (float) $resource->amount_needed;
-                    $totalFound += (float) ($resource->amount_found ?? 0);
-                }
-            }
-
-            // 5. set progress to 0 if totalNeeded is 0 to avoid division by zero
-            if ($totalNeeded <= 0) {
-                $project['progress'] = 0;
-
-                continue;
-            }
-
-            // 6. calculate progress percentage
-            $progress = ($totalFound / $totalNeeded) * 100;
-
-            // 7. round progress to 2 decimal places and add it to the project object
-            $progress = round($progress, 2);
-            $project['progress'] = $progress;
-        }
-
         return view('dashboard', ['projects' => $projects]);
     }
 }
