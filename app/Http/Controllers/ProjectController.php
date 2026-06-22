a582abf0ac0b9544125831d8c66b9eadb0cf9134
nekrosb <makarov.nikita.mn@gmail.com>
2026-06-22 16:22:47 +0200

    fix: take file from main

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,19 +2,53 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Project;
-use App\Models\User
-
-
+use App\Models\States\ApprovedState;
+use App\Models\States\ModificationState;
+use App\Models\States\RefusedState;
+use App\Models\States\SubmittedState;
+use App\Models\User;
+use Illuminate\Support\Facades\Redirect;
 
 class ProjectController extends Controller
 {
     public function index()
     {
         $projects = Project::select('id', 'title', 'description')->with('evaluation')->get();
-        $user = User::select('id', 'name')->get();
+        $users = User::select('id', 'name')->get();
 
-        return view('dashboard', compact('projects', 'user'));
+        return view('allProjects', ['projects' => $projects, 'users' => $users]);
+    }
+
+    public function approve(Project $project)
+    {
+        $project->status->transitionTo(ApprovedState::class);
+        $project->save();
+
+        return Redirect::back()->with('status', 'project-approved');
+    }
+
+    public function deny(Project $project)
+    {
+        $project->status->transitionTo(RefusedState::class);
+        $project->save();
+
+        return Redirect::back()->with('status', 'project-denied');
+    }
+
+    public function requestMoreInfo(Project $project)
+    {
+        $project->status->transitionTo(ModificationState::class);
+        $project->save();
+
+        return Redirect::back()->with('status', 'more-info-requested');
+    }
+
+    public function reSubmit(Project $project)
+    {
+        $project->status->transitionTo(SubmittedState::class);
+        $project->save();
+
+        return Redirect::back()->with('status', 'project-resubmitted');
     }
 }
