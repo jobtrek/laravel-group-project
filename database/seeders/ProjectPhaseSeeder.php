b822a2f48981abc125331ab115ac9da06a7ebf94
nekrosb <makarov.nikita.mn@gmail.com>
2026-06-22 14:19:15 +0200

    fix: rename file and classes

--- database/seeders/ProjectPhaseSeeder.php
+++ database/seeders/ProjectPhaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class ProjectPhaseFactory extends Seeder
+class ProjectPhaseSeeder extends Seeder
 {
     /**
      * Run the database seeds.
