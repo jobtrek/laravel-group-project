5095921d4c9e5ba85db87c43325ad47fd34f1ae7
thomaslucking <telucking@gmail.com>
2026-06-22 10:10:25 +0200

    style: formatted code

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,12 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PropositionController;
-use App\Mail\MailableName;
 use App\Models\User;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
 
 Route::get('/dashboard', function () {
     return redirect()->route('projects');
