3fa53272ca0dc7615222d6b2c0197d29eaaeb6e9
FrstF4ll <hydralixe@gmail.com>
2026-06-24 14:59:48 +0200

    fix: remove name from porteur field html element

--- resources/views/components/proposition/step-1.blade.php
+++ resources/views/components/proposition/step-1.blade.php
@@ -10,8 +10,7 @@
         <div>
             <label class="block text-sm font-medium text-gray-700">Porteur</label>
             <p class="text-xs text-gray-500 mb-1">Personne garantissant cette proposition</p>
-            <p name="porteur" x-model="porteur"
-                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
+            <p class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
                 {{auth()->user()->name}}
             </p>
             <p x-show="errors.porteur" x-text="errors.porteur || ''" class="mt-1 text-sm text-red-600"></p>
