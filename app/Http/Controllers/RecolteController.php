e41242016bdec18d3d630b595a2489349c698804
nekrosb <makarov.nikita.mn@gmail.com>
2026-06-25 13:49:36 +0200

    feat(backend): user can move project from recolte to active

---
+++ app/Http/Controllers/RecolteController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Project;
+use App\Models\States\ActiveState;
+
+
+
+class RecolteController extends Controller
+{
+    public function moveFromRecoltToActiv(Request $request)
+    {
+        $recolteId = $request->input('recolte_id');
+        $project = Project::find($recolteId);
+
+        if ($project->progress > 80) {
+            $project->status = new ActiveState();
+            $project->save();
+
+            return redirect()->back()->with('success', 'Project moved to Active state successfully.');
+        } else {
+            return redirect()->back()->with('error', 'Project cannot be moved to Active state. Progress must be greater than 80%.');
+        }
+    }
+}
