d86f084b435e5ba0216dbe0885d7a8f7802305c1
wasp32679 <ryan.kenmoe@outlook.fr>
2026-06-26 08:43:52 +0200

    chore: lint

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -149,8 +149,8 @@ class Project extends Model
 
         foreach ($this->phases as $phase) {
             foreach ($phase->resources as $resource) {
-                $totalNeeded += (float)$resource->amount_needed;
-                $totalFound += (float)($resource->amount_found ?? 0);
+                $totalNeeded += (float) $resource->amount_needed;
+                $totalFound += (float) ($resource->amount_found ?? 0);
             }
         }
 
