6b4fa7a9411ce2324985f226818b38fb84dab1d2
joao aebi <joao.aebi@jobtrek.ch>
2026-06-29 10:13:48 +0200

    feat: changed colors for status and removed empty props

--- resources/views/components/projects_Details/inputAddtask.blade.php
+++ resources/views/components/projects_Details/inputAddtask.blade.php
@@ -1,7 +1,3 @@
-@props([
-
-])
-
 <div class="mt-1 flex gap-3 rounded-lg p-3">
     <input type="text" placeholder="Titre du nouveau but..."
            class="w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm text-gray-900 placeholder-gray-400 focus:border-indigo-700 focus:outline-none focus:ring-1">
