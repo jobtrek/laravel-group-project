d21173bf67850fdf42e02357769d2276d4c51bd8
thomaslucking <telucking@gmail.com>
2026-06-29 11:40:37 +0200

    refactor: refactored routes to use laravel conventions

--- routes/web.php
+++ routes/web.php
@@ -13,42 +13,22 @@ use App\Models\States\PropositionState;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', fn () => view('welcome'));
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', fn () => redirect()->route('projects'))->name('dashboard');
+    Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
+    Route::get('/propositions', [PropositionController::class, 'index'])->name('propositions');
+    Route::get('/evaluation', [ReviewController::class, 'index'])->name('evaluation');
+    Route::get('/recolte', [RecolteController::class, 'index'])->name('recolte');
+    Route::get('/en-cours', [EnCoursController::class, 'index'])->name('en-cours');
+    Route::get('/frigo', [ArchiveController::class, 'index'])->name('frigo');
 });
 
-Route::get('/dashboard', function () {
-    return redirect()->route('projects');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/projects', [ProjectController::class, 'index'])
-    ->middleware(['auth', 'verified'])->name('projects');
-
-Route::get('/propositions', [PropositionController::class, 'index'])
-    ->middleware(['auth', 'verified'])->name('propositions');
-
-Route::get('/evaluation', [ReviewController::class, 'index'])
-    ->middleware(['auth', 'verified'])->name('evaluation');
-
-Route::get('/recolte', [RecolteController::class, 'index'])
-    ->middleware(['auth', 'verified'])->name('recolte');
-
-Route::get('/en-cours', [EnCoursController::class, 'index'])
-    ->middleware(['auth', 'verified'])->name('en-cours');
-
-Route::get('/frigo', [ArchiveController::class, 'index'])
-    ->middleware(['auth', 'verified'])->name('frigo');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/create', function () {
-        return view('create', ['users' => User::query()->select('id', 'name')->get()]);
-    })->name('create');
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::post('/propositions', [PropositionController::class, 'store'])->name('proposition.store');
+    Route::get('/create', fn () => view('create', [
+        'users' => User::query()->select('id', 'name')->get(),
+    ]))->name('create');
 
     Route::get('/direction/projects', function () {
         $projects = Project::with('evaluation')
@@ -58,10 +38,20 @@ Route::middleware('auth')->group(function () {
         return view('testDirectionFront', ['projects' => $projects]);
     })->name('direction.projects');
 
-    Route::patch('/projects/{project}/approve', [ProjectController::class, 'approve'])->name('projects.approve');
-    Route::patch('/projects/{project}/deny', [ProjectController::class, 'deny'])->name('projects.deny');
-    Route::post('/projects/{project}/request-more-info', [ProjectController::class, 'requestMoreInfo'])->name('projects.request-more-info');
-    Route::patch('/projects/{project}/resubmit', [ProjectController::class, 'reSubmit'])->name('projects.resubmit');
+    Route::post('/propositions', [PropositionController::class, 'store'])->name('proposition.store');
+
+    Route::controller(ProfileController::class)->group(function () {
+        Route::get('/profile', 'edit')->name('profile.edit');
+        Route::patch('/profile', 'update')->name('profile.update');
+        Route::delete('/profile', 'destroy')->name('profile.destroy');
+    });
+
+    Route::controller(ProjectController::class)->prefix('/projects/{project}')->group(function () {
+        Route::patch('/approve', 'approve')->name('projects.approve');
+        Route::patch('/deny', 'deny')->name('projects.deny');
+        Route::post('/request-more-info', 'requestMoreInfo')->name('projects.request-more-info');
+        Route::patch('/resubmit', 'reSubmit')->name('projects.resubmit');
+    });
 });
 
 require __DIR__.'/auth.php';
