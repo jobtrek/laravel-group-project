1a5304937c1fefca51cdcc1ecbf6b72501526e54
Thomas Lucking <128782962+ThomasLucking@users.noreply.github.com>
2026-06-29 13:37:12 +0200

    fix: updated states in factories/migrations/seeders (#128)
    * fix: updated states in factories/migrations/seeders
    
    * refactor: refactored routes to use laravel conventions
    
    * fix: details is now visible for the frontend
    
    * fix: updated states in commentFactory
    
    * fix: re added project-details route

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,11 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Enums\Stage;
 use App\Models\Comment;
 use App\Models\Project;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use App\Models\States\RevisionState;
+use App\Models\States\EncoursState;
 /**
  * @extends Factory<Comment>
  */
@@ -21,7 +23,7 @@ class CommentFactory extends Factory
     {
         return [
             'content' => $this->faker->paragraph(),
-            'stage' => $this->faker->randomElement(['planning', 'development', 'testing', 'deployment']),
+            'stage' => $this->faker->randomElement([RevisionState::getMorphClass(), EncoursState::getMorphClass()]),
             'user_id' => User::factory(),
             'project_id' => Project::factory(),
         ];
