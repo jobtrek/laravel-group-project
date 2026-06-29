99cf94e88addc37d425e74db774407286e9a7b76
FrstF4ll <hydralixe@gmail.com>
2026-06-29 15:17:34 +0200

    feat: build seeders for roles and custom users
    - Update database seeder to initialize role and permission

---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $permissions = [
+            'view projects',
+            'access direction page',
+            'validate project',
+            'refuse project',
+            'return project',
+        ];
+
+        foreach ($permissions as $permission){
+            Permission::firstOrCreate([
+                'name' => $permission,
+                'guard_name' => 'web']);
+        }
+
+        $userRole = Role::firstOrCreate(['name' => 'user', 'guard_name' => 'web']);
+        $adminRole = Role::firstOrCreate(['name' => 'admin', 'guard_name' => 'web']);
+        $directionRole = Role::firstOrCreate(['name' => 'direction', 'guard_name' => 'web']);
+
+        $userRole->givePermissionTo('view projects');
+        $adminRole->givePermissionTo(Permission::all());
+
+        $directionRole->givePermissionTo(['access direction page',
+            'validate project',
+            'refuse project',
+            'return project'
+        ]);
+    }
+}
