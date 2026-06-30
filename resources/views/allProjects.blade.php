f6766d173178fd6a4e7fe8380ab5c3af7d89a785
wasp32679 <ryan.kenmoe@outlook.fr>
2026-06-30 11:30:36 +0200

    feat: frontend and state machine are linked

--- resources/views/allProjects.blade.php
+++ resources/views/allProjects.blade.php
@@ -5,23 +5,32 @@
             <div class=" bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <div class="flex justify-between gap-4 mb-4">
-                        <x-projects.countProjects text="Propositions" :projets="$counts->get('proposition', 0) + $counts->get('révision', 0)" route="propositions" />
+                        <x-projects.countProjects text="Propositions"
+                                                  :projets="$counts->get('proposition', 0) + $counts->get('révision', 0)"
+                                                  route="propositions"/>
                         <x-projects.countProjects text="Evaluation" :projets="$counts->get('évaluation', 0)"
-                            route="evaluation" />
+                                                  route="evaluation"/>
                         <x-projects.countProjects text="Récolte" :projets="$counts->get('récolte', 0)"
-                            route="recolte" />
+                                                  route="recolte"/>
                         <x-projects.countProjects text="En cours" :projets="$counts->get('en cours', 0)"
-                            route="en-cours" />
-                        <x-projects.countProjects text="Frigo" :projets="$counts->get('archivé', 0) + $counts->get('complété', 0)" route="frigo" />
+                                                  route="en-cours"/>
+                        <x-projects.countProjects text="Frigo"
+                                                  :projets="$counts->get('archivé', 0) + $counts->get('complété', 0)"
+                                                  route="frigo"/>
                     </div>
                     <a href="{{ route('create') }}" class="bg-blue-700 text-white rounded-lg p-1">New proposal</a>
                     <div class="flex flex-col gap-4 mt-4">
                         @foreach($projects as $project)
-                            @if($project->status != 'archivé')
-                                <x-projects.displayProjects :project="$project" :status="$project->status" :title="$project->title"
-                                    :chef="$project->leader?->name ?? $project->proposer?->name" :progress="$project->progress"
-                                    :importance="$project->importance" :creation-date="$project->created_at->format('d M Y')"
-                                    :updated_at="$project->updated_at" />
+                            @if($project && $project->status != 'archivé')
+                                <x-projects.displayProjects
+                                        :project="$project"
+                                        :status="$project->status"
+                                        :title="$project->title"
+                                        :chef="$project->leader?->name ?? $project->proposer?->name"
+                                        :progress="$project->progress"
+                                        :importance="$project->importance"
+                                        :creation-date="$project->created_at->format('d M Y')"
+                                        :updated_at="$project->updated_at"/>
                             @endif
                         @endforeach
                     </div>
