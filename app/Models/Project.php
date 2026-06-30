5a7fe94141d18e8d6070e8ed01ef703b1730af63
Thomas Lucking <128782962+ThomasLucking@users.noreply.github.com>
2026-06-30 15:51:14 +0200

    refactor: deleted useless static method (#158)
    * refactor: deleted useless static method
    
    * fix: replaced is() with instance of

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -95,52 +95,6 @@ class Project extends Model
         return $this->evaluation?->importance;
     }
 
-    public static function createProposal(array $data, int $proposerId): self
-    {
-        return DB::transaction(function () use ($data, $proposerId) {
-            $project = self::create([
-                'title' => $data['titre'],
-                'description' => $data['description'],
-                'but' => $data['buts'],
-                'perimetre' => $data['perimetre'] ?? null,
-                'ressources_totales' => $data['ressources_totales'] ?? null,
-                'status' => PropositionState::getMorphClass(),
-                'current_stage' => PropositionState::getMorphClass(),
-                'proposer_id' => $proposerId,
-                'leader_id' => $data['porteur'],
-            ]);
-
-            $project->members()->attach($data['membres']);
-
-            $project->evaluation()->create([
-                'portee' => $data['portee'],
-                'impact' => $data['impact'],
-                'confiance' => $data['confiance'],
-                'effort' => $data['effort'],
-            ]);
-
-            foreach ($data['phases'] as $index => $phase) {
-                /** @var ProjectPhase $createdPhase */
-                $createdPhase = $project->phases()->create([
-                    'name' => $phase['titre'],
-                    'duration' => $phase['duree'],
-                    'description' => $phase['description'],
-                    'objectifs' => $phase['objectifs'],
-                    'livrables' => $phase['livrables'],
-                    'order' => $index + 1,
-                ]);
-
-                foreach ($phase['ressources_necessaires'] as $resource) {
-                    $createdPhase->resources()->create([
-                        'resource_type' => $resource['resource_type'],
-                        'amount_needed' => $resource['amount_needed'],
-                    ]);
-                }
-            }
-
-            return $project;
-        });
-    }
 
     public function getProgressAttribute(): float
     {
