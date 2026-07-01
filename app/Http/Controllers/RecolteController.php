297008fff361c38eb8240fd3a1297cf693ff8e57
thomaslucking <telucking@gmail.com>
2026-07-01 10:52:14 +0200

    refactor: extracted the index methods into an abtract class

--- app/Http/Controllers/RecolteController.php
+++ app/Http/Controllers/RecolteController.php
@@ -3,36 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Enums\Stage;
-use App\Filters\ProjectFilter;
-use App\Http\Requests\FilterProjectsRequest;
 use App\Models\PhaseResource;
 use App\Models\Project;
 use App\Models\States\EncoursState;
 use App\Models\States\RecolteState;
-use App\Models\User;
 use Illuminate\Http\Request;
 
-class RecolteController extends Controller
+class RecolteController extends StageProjectController
 {
-    public function __construct(
-        private readonly ProjectFilter $filter
-    ) {}
-
-    public function index(FilterProjectsRequest $request)
+    protected function stage(): Stage
     {
-        $projects = $this->filter->apply(
-            Project::with(['proposer', 'leader', 'evaluation'])
-                ->whereState('status', [RecolteState::class]),
-            $request
-        )->paginate(10);
+        return Stage::Recolte;
+    }
 
-        $users = User::query()->select('id', 'name')->orderBy('name')->get();
-
-        return view('stage', [
-            'stage' => Stage::Recolte,
-            'projects' => $projects,
-            'users' => $users,
-        ]);
+    protected function states(): string|array
+    {
+        return [RecolteState::class];
     }
 
     public function moveFromRecolteToActive(Request $request)
--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,34 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Enums\Stage;
-use App\Filters\ProjectFilter;
-use App\Http\Requests\FilterProjectsRequest;
 use App\Models\Project;
 use App\Models\States\EvaluationState;
-use App\Models\User;
 use Illuminate\View\View;
 
-class ReviewController extends Controller
+class ReviewController extends StageProjectController
 {
-    public function __construct(
-        private readonly ProjectFilter $filter
-    ) {}
-
-    public function index(FilterProjectsRequest $request)
+    protected function stage(): Stage
     {
-        $projects = $this->filter->apply(
-            Project::with(['proposer', 'leader', 'evaluation'])
-                ->whereState('status', [EvaluationState::class]),
-            $request
-        )->paginate(10);
+        return Stage::Evaluation;
+    }
 
-        $users = User::query()->select('id', 'name')->orderBy('name')->get();
-
-        return view('stage', [
-            'stage' => Stage::Evaluation,
-            'projects' => $projects,
-            'users' => $users,
-        ]);
+    protected function states(): string|array
+    {
+        return [EvaluationState::class];
     }
 
     public function showForm(Project $project): View
