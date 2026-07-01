055f8c138733b2a1484c6367522945922babff16
Thomas Lucking <128782962+ThomasLucking@users.noreply.github.com>
2026-07-01 13:52:43 +0200

    perf: added phases.resources to eager loading (#175)
    * perf: added phases.resources to eager loading
    
    * perf: added phases.contributions to the eager loading
    
    * style: formatted code

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -24,11 +24,11 @@ class ProjectController extends Controller
     public function index(FilterProjectsRequest $request)
     {
         $projects = $this->filter->apply(
-            Project::with(['proposer', 'leader', 'evaluation']),
-            $request
+            Project::with(['proposer', 'leader', 'evaluation', 'phases.resources', 'phases.contributions']), $request
         )->paginate(10)->withQueryString();
 
         $counts = Project::statusCounts();
+
         $users = User::query()->select('id', 'name')->orderBy('name')->get();
 
         return view('allProjects', compact('projects', 'counts', 'users'));
