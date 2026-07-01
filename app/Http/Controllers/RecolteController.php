8ab1259ecd6c8f7f8384364c1b300dd3a8e19597
thomaslucking <telucking@gmail.com>
2026-07-01 13:49:20 +0200

    style: formatted code

--- app/Http/Controllers/RecolteController.php
+++ app/Http/Controllers/RecolteController.php
@@ -23,7 +23,7 @@ class RecolteController extends StageProjectController
     public function moveFromRecolteToActive(Project $project): RedirectResponse
     {
         $project->loadMissing(['phases.resources', 'phases.contributions']);
-        
+
         if (! $project->status instanceof RecolteState) {
             return redirect()->back()->with('error', 'Project is not in Recolte state.');
         }
