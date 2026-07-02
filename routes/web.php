66e5d82ed42dac02efe601270240db50b1874923
FrstF4ll <hydralixe@gmail.com>
2026-07-02 10:28:53 +0200

    fix: add middleware for direction review pages.
    - Prevent users to access direction review page

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,25 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/recolte', [RecolteController::class, 'index'])->name('recolte');
     Route::get('/en-cours', [EnCoursController::class, 'index'])->name('en-cours');
     Route::get('/frigo', [ArchiveController::class, 'index'])->name('frigo');
-    Route::get('/projects/{project}/direction-review', [ReviewController::class, 'showForm'])->name('projects.direction-review');
-    Route::get('/projects/{project}/revision', [RevisionController::class, 'showForm'])->name('projects.revision-form');
-    Route::post('/projects/{project}/revision-submit', [RevisionController::class, 'submit'])->name('projects.revision-submit');
+    Route::get('/projects/{project}/direction-review', [ReviewController::class, 'showForm'])
+        ->middleware('can:review')
+        ->name('projects.direction-review');
+    Route::get('/projects/{project}/revision', [RevisionController::class, 'showForm'])
+        ->middleware('can:review')
+        ->name('projects.revision-form');
+    Route::post('/projects/{project}/revision-submit', [RevisionController::class, 'submit'])
+        ->middleware('can:review')
+        ->name('projects.revision-submit');
 });
 
-Route::get('/projects_details/{project}', [ProjectController::class, 'detailPage'])->middleware(['auth', 'verified'])->name('projects-details');
+Route::get('/projects_details/{project}', [ProjectController::class, 'detailPage'])
+    ->middleware(['auth', 'verified'])
+    ->name('projects-details');
 
 Route::get('/phase_details/{phase}', function (ProjectPhase $phase) {
     return view('phase_details', compact('phase'));
-})->middleware(['auth', 'verified'])->name('phase_details');
+})->middleware(['auth', 'verified'])
+    ->name('phase_details');
 
 Route::middleware('auth')->group(function () {
     Route::get('/create', fn () => view('create', [
@@ -57,7 +66,9 @@ Route::middleware('auth')->group(function () {
         Route::patch('/review', 'review')->middleware('can:review')->name('projects.review');
     });
 
-    Route::controller(ResourceContributionController::class)->prefix('/projects/{project}/resources')->middleware('can:add resources')->group(function () {
+    Route::controller(ResourceContributionController::class)->prefix('/projects/{project}/resources')
+        ->middleware('can:add resources')->
+        group(function () {
         Route::get('/create', 'create')->name('projects.resources.create');
         Route::post('/', 'store')->name('projects.resources.store');
     });
