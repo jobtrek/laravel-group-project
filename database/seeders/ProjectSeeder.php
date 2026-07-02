c5e60f64c2f2807b03cdba41bd8c54fea222bdb7
FrstF4ll <hydralixe@gmail.com>
2026-07-02 08:46:38 +0200

    perf: nest project seeder in a transaction

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\PhaseResource;
 use App\Models\Project;
+use App\Models\ProjectPhase;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,6 +14,15 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        Project::factory(10)->create();
+        DB::transaction(function () {
+            Project::factory(123)
+                ->has(
+                    ProjectPhase::factory()
+                        ->count(3)
+                        ->has(PhaseResource::factory()->count(2), 'resources'),
+                    'phases'
+                )
+                ->create();
+        });
     }
 }
