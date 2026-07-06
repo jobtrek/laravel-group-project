f593abe2b476158b784a057d2243f09fef44783c
Thomas Lucking <128782962+ThomasLucking@users.noreply.github.com>
2026-07-06 08:20:29 +0200

    fix: removed useless account deletion (#239)
    * fix: removed useless account deletion
    
    * style: formatted and linted code

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,6 @@ Route::middleware('auth')->group(function () {
     Route::controller(ProfileController::class)->group(function () {
         Route::get('/profile', 'edit')->name('profile.edit');
         Route::patch('/profile', 'update')->name('profile.update');
-        Route::delete('/profile', 'destroy')->name('profile.destroy');
     });
 
     Route::controller(ProjectController::class)->prefix('/projects/{project}')->group(function () {
