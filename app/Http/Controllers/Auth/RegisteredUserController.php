09f2acaabbf8c78b681345b154734f91ba54ade5
FrstF4ll <tizdimo@gmail.com>
2026-07-06 15:50:56 +0200

    refactor: remove redudant auth check

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,10 +44,8 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        if (Auth::check()) {
-            return redirect()->route('register')
-                ->with('status', __('Utilisateur créé avec succès.'));
-        }
+        return redirect()->route('register')
+            ->with('status', __('Utilisateur créé avec succès.'));
 
         Auth::login($user);
 
