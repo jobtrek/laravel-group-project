4f126a93e6143b939139723098ec8ae13e8f1a88
FrstF4ll <tizdimo@gmail.com>
2026-07-07 14:18:07 +0200

    fix: remove unwanted semicolon after endphp

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,16 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+@props([
+    'color' => 'green'
+])
+
+@php
+    $themes = match($color) {
+        'black' => '',
+        'green' => 'border-green-200 bg-green-50text-green-600 hover:bg-green-100',
+        default => 'border-green-200 bg-green-50text-green-600 hover:bg-green-100'
+    }
+@endphp
+<button {{ $attributes->merge(['type' => 'submit',
+'class' => 'p-2 inline-block mb-4 rounded-lg border
+px-4.5 py-2 text-sm font-medium transition-colors' . $themes]) }}>
     {{ $slot }}
 </button>
