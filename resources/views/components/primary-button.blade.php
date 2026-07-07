1915ad237ba2985166ce091faaa782f3f9c912c8
FrstF4ll <tizdimo@gmail.com>
2026-07-07 15:52:32 +0200

    fix: add space to correctly apply theme
    - Add semicolon to end 'theme' declaration

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -7,10 +7,10 @@
         'black' => '',
         'green' => 'border-green-200 bg-green-50text-green-600 hover:bg-green-100',
         default => 'border-green-200 bg-green-50text-green-600 hover:bg-green-100'
-    }
+    };
 @endphp
 <button {{ $attributes->merge(['type' => 'submit',
 'class' => 'p-2 inline-block mb-4 rounded-lg border
-px-4.5 py-2 text-sm font-medium transition-colors' . $themes]) }}>
+px-4.5 py-2 text-sm font-medium transition-colors ' . $themes]) }}>
     {{ $slot }}
 </button>
