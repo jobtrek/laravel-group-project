389c1f4eacd6b293dbc010c607a161bbab080f42
FrstF4ll <tizdimo@gmail.com>
2026-07-07 11:42:16 +0200

    feat: include collaborator by default on user creation
    - ALl user created by admin are 'collaborateur' + any role

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,7 +43,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-        $user->assignRole(Role::from($request->role));
+        $user->assignRole([Role::Collaborateur, Role::from($request->role)]);
 
         event(new Registered($user));
 
