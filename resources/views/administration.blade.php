1d22ede8ad2bd2a8bea2c5f5ac50bff30b994bff
joao aebi <joao.aebi@jobtrek.ch>
2026-07-07 16:14:57 +0200

    feat: deleted white banner

--- resources/views/administration.blade.php
+++ resources/views/administration.blade.php
@@ -2,7 +2,7 @@
 use App\Enums\Role;
 @endphp;
 <x-app-layout>
-    <div class="bg-[#131c3f]">
+    <div>
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
             <div class="flex items-center justify-between">
                 <h1 class="text-2xl font-bold text-white">Administration</h1>
