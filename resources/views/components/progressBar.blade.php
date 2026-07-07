cd1ef002f9db36653cd0d77b21cf1247530fd798
joao aebi <joao.aebi@jobtrek.ch>
2026-07-07 11:38:16 +0200

    feat: chaged colors

--- resources/views/components/progressBar.blade.php
+++ resources/views/components/progressBar.blade.php
@@ -16,11 +16,11 @@ use App\Http\Controllers\ProjectController;
 <div class="relative z-10 pointer-events-none flex items-center gap-2 w-full">
     <div class="w-full bg-gray-200 rounded-full h-1.5 flex overflow-hidden">
 
-        <div class="{{ $progress <= 20 ? 'bg-red-500' : 'bg-emerald-500' }} h-full"
+        <div class="{{ $progress <= 20 ? 'bg-red-500' : 'bg-emerald-400' }} h-full"
              style="width: {{ min($progress, 100) }}%"></div>
 
         @if($progress > 100)
-            <div class="bg-cyan-300 h-full"
+            <div class="bg-blue-500 h-full"
                  style="width: {{ $progress - 100 }}%"></div>
         @endif
 
