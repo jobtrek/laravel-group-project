55ba0161b7f783b054c51c7711ca95e80f605735
Thomas Lucking <128782962+ThomasLucking@users.noreply.github.com>
2026-08-12 09:46:56 +0200

    refactor(frontend): removed useless props (#389)
    * fix: when assigning a team, the role chef du project is assigned
    
    * refactor: removed redundant props and extracted logic

--- resources/views/allProjects.blade.php
+++ resources/views/allProjects.blade.php
@@ -34,15 +34,7 @@
                     </div>
                     <div class="flex flex-col gap-3.5">
                         @foreach($projects as $project)
-                            <x-projects.displayProjects
-                                    :project="$project"
-                                    :status="$project->status"
-                                    :title="$project->title"
-                                    :chef="$project->leader?->name ?? $project->proposer?->name"
-                                    :progress="$project->progress"
-                                    :importance="$project->importance"
-                                    :creation-date="$project->created_at?->locale('fr')?->translatedFormat('d M Y') ?? '—'"
-                                    :updated_at="$project->updated_at"/>
+                            <x-projects.displayProjects :project="$project"/>
                         @endforeach
                     </div>
 
