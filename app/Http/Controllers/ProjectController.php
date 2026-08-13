77895e4332f845b012741c84c748b91c6056faed
thomaslucking <telucking@gmail.com>
2026-08-13 10:25:03 +0200

    style: formatted code

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -19,6 +19,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Redirect;
 use Spatie\ModelStates\Exceptions\CouldNotPerformTransition;
+
 class ProjectController extends Controller
 {
     public function __construct(
